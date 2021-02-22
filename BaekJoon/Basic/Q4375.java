a9ba1f469938ed960efbb22582f03c112cb8833e
Ma-due <maduee2@naver.com>
2021-02-22 19:59:58 +0900

    Brute Force 1(4375)

---
+++ BaekJoon/Basic/Q4375.java
@@ -0,0 +1,24 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Q4375 {
+
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		while(sc.hasNextInt()) {
+			int n = sc.nextInt();
+			int num = 0;
+			
+			for(int i = 1 ;; i++) {
+				num = num * 10 + 1;
+				num %= n;
+				if(num == 0) {
+					System.out.println(i);
+					break;
+				}
+			}
+		}
+	}
+}
+
